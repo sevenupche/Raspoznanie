4ebe4e55518af535c633d73b467e7a94fcd8296d
Sevenup72 <sevenup@ya.ru>
2016-08-10 22:38:50 +0600

    -----------
    class Vector

---
+++ Vector.java
@@ -0,0 +1,15 @@
+package AllClass;
+
+public class Vector {
+
+	public Vector(int x_, int y_, int hv_, int longhv_, int pos_) {
+		// TODO Auto-generated constructor stub
+		x=x_; y=y_; hv=hv_; longhv=longhv_; pos=pos_;
+	}
+	public int x;
+	public int y;
+	public int hv;//napravlenie
+	public int longhv;//dlina
+	public int pos;//poziciay izvectnoy bykvy v slove
+	
+}
